0183fb7a20e35913645fbf29b855380c108ee1ba
jyotibora-sap <110669095+jyotibora-sap@users.noreply.github.com>
2023-07-28 15:51:36 +0530

    Update streamlit_app.py
    multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,9 @@ streamlit.text(' 🥣Omega3 and Blueberry Oatmeal')
 streamlit.text(' 🥗Kale,Spinach and Rocket smoothie')
 streamlit.text(' 🐔Hard Boiled Free Range Eggs')
 streamlit.text(' 🥑🍞Hard Boiled Free Range Eggs')
+# Let's put a pick list here so they can pick the fruit they want to include 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
