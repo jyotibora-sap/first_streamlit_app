316d44d3a15711cb34517ecc0a721cf421e4a871
jyotibora-sap <110669095+jyotibora-sap@users.noreply.github.com>
2023-07-28 15:48:38 +0530

    Update streamlit_app.py
    added pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text(' 🥣Omega3 and Blueberry Oatmeal')
@@ -6,3 +10,4 @@ streamlit.text(' 🥗Kale,Spinach and Rocket smoothie')
 streamlit.text(' 🐔Hard Boiled Free Range Eggs')
 streamlit.text(' 🥑🍞Hard Boiled Free Range Eggs')
 
+streamlit.dataframe(my_fruit_list)
