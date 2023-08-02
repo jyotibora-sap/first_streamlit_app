a0e83e7649755e27359fc26c55b94420c1305523
jyotibora-sap <110669095+jyotibora-sap@users.noreply.github.com>
2023-08-02 17:38:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 
-stramlit.text("The fruit ")
+streamlit.text("The fruit ")
 streamlit.text(my_data_row)
 
 
