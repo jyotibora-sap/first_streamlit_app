2c9e536b15f0b037fd9a1a74d89a1c107bbf32b5
jyotibora-sap <110669095+jyotibora-sap@users.noreply.github.com>
2023-08-03 16:02:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ def get_fruityvice_data(this_fruit_choice):
 
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
     # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     # write your own comment - what does this do?
-    return fruityvice_normalized
+  return fruityvice_normalized
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
