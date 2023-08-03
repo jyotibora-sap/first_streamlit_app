bd754baf210e783f79615ae98b1fb8def6ba6653
jyotibora-sap <110669095+jyotibora-sap@users.noreply.github.com>
2023-08-03 16:04:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,9 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error
+
 def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
     return my_cur.fetchall();
 
@@ -55,6 +56,7 @@ def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
   my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from Steamlit')")
   return "Thanks for adding" : +new_fruit
+
 fruit_to_add = streamlit.text_input('What fruit would you like to eat')
 if streamlit.button('Add a fruit to the List')
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
